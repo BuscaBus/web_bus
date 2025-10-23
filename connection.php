2ea3fa5e3123ae56da542c9d1c84aae82006f974
BuscaBus <52289859+BuscaBus@users.noreply.github.com>
2025-10-22 21:17:35 -0300

    Atualização da conexão ao banco de dados

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
     $server = "localhost";
     $user = "root";
     $password = "";
-    $dbname = "web_bus_bd"; 
+    $dbname = "web_bus_bd_v3"; 
     
     // criar conexão 
     $conexao = mysqli_connect($server, $user, $password, $dbname);
